fc985b2272bedfdc5bbd63822eb22d06fff2fb69
ChristopherStanley14 <christopherstanley@ChristopherStanleyMBP.local>
2016-11-01 16:59:48 -0400

    Updated Storyboards

---
+++ ViewControllers/LoginViewController.swift
@@ -0,0 +1,45 @@
+//
+//  LoginViewController.swift
+//  EFAB
+//
+//  Created by Christopher Stanley on 11/1/16.
+//  Copyright © 2016 Christopher Stanley. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    
+    // MARK: - IBAction
+    
+    @IBAction func loginButton(_ sender: Any) {
+    }
+    @IBAction func registerButton(_ sender: Any) {
+    }
+}
